87677394f566fb76c598743e111de145819e83c3
ShaurGaur <shauryavrat@live.com>
2024-10-14 16:56:31 +0200

    plot category perf nlst

--- utils.py
+++ utils.py
@@ -53,3 +53,34 @@ def plot_rocs(rocs, title=None, imgpath=None, plot_ci=False, figsize=(6,6)):
         plt.savefig(imgpath, dpi=600)
     plt.show()
 
+def ax_rocs(ax, rocs, title=None, plot_ci=False):
+    ax.plot([0.0, 1.0], [0.0, 1.0], "--", color="k", alpha=0.5)
+    ax.set_xlabel('False Positive Rate', fontsize=14)
+    ax.set_ylabel('True Positive Rate', fontsize=14)
+    ax.set_xticks(np.arange(0, 1.1, 0.1), np.around(np.arange(0, 1.1, 0.1), 1), fontsize=12)  # X axis ticks in steps of 0.1
+    ax.set_yticks(np.arange(0, 1.1, 0.1), np.around(np.arange(0, 1.1, 0.1), 1), fontsize=12)  # Y axis ticks in steps of 0.1
+    ax.grid(lw=1)
+    ax.set_xlim(-0.005,1)
+    ax.set_ylim(0,1.005)
+
+    for i, label in enumerate(rocs):
+        roc = rocs[label]
+        # roc = get_bootstrapped_roc_ci_curves(df[MODEL_TO_COL[m]].values, df[true_col].values)
+        auc = skl_metrics.auc(roc.fpr_vals, roc.mean_tpr_vals)
+
+        ax.plot(
+            roc.fpr_vals, roc.mean_tpr_vals, color=color_palette[i],
+            label=f"{label}: AUC = {auc:.3f} ({roc.low_az_val:.3f} - {roc.high_az_val:.3f})",
+        )
+        if plot_ci:
+            ax.fill_between(roc.fpr_vals, roc.low_tpr_vals, roc.high_tpr_vals, color=color_palette[i], alpha=.1)
+
+    if title:
+        ax.set_title(title, fontsize=14)
+
+    leg = ax.legend(loc='lower right', fontsize=12)
+    # shift = max([t.get_window_extent().width for t in leg.get_texts()])
+    # for t in leg.get_texts():
+    #     t.set_ha('right') # ha is alias for horizontalalignment
+    #     t.set_position((shift - t.get_window_extent().width,0))
+
